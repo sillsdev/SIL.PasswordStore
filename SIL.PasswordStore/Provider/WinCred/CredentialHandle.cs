66e8ceb795e19da91d7013baab57052d4501e2e7
Eberhard Beilharz <eb1@sil.org>
2026-08-13 12:16:21 +0200

    Update nuget packages and target versions
    - update organization name
    - update nuget packages
    - add net10 and net462 as targets
    - remove net461 and net6 targets

--- SIL.PasswordStore/Provider/WinCred/CredentialHandle.cs
+++ SIL.PasswordStore/Provider/WinCred/CredentialHandle.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2022 SIL International
+﻿// Copyright (c) 2022-2026 SIL Global
 // This software is licensed under the MIT License (http://opensource.org/licenses/MIT)
 
 using System;
